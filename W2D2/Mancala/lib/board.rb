1c783b590033a174451072665ac9d5d96fcd8b33
Nick Battjes <nhbattjes@gmail.com>
2018-07-11 08:46:23 -0700

    rename folder to correct day

--- W2D2/Mancala/lib/board.rb
+++ W2D2/Mancala/lib/board.rb
@@ -2,17 +2,18 @@ class Board
   attr_accessor :cups, :stone, :current_player
 
   def initialize(name1, name2)
-    @cups = Array.new(14) {Array.new(0)}
-    self.place_stones
-    @current_player = @player1
+    @cups = Array.new(14) { Array.new }
+    place_stones
+    @name1 = name1
+    @name2 = name2
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
     4.times do |i|
-    self.cups[0..5].each { |cup| cup << :stone }
-    self.cups[7..12].each { |cup| cup << :stone }
-  end
+      self.cups[0..5].each { |cup| cup << :stone }
+      self.cups[7..12].each { |cup| cup << :stone }
+    end
   end
 
   class InvalidCupError < StandardError
@@ -28,26 +29,35 @@ class Board
   end
 
   def valid_move?(start_pos)
-    raise InvalidCupError unless start_pos.between?(0, 13)
+    raise InvalidCupError unless start_pos.between?(0, 12)
     raise StartingCupError if cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
-    stone_count = cups[start_pos].count
-    cups[start_pos] = []
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
     current_cup = start_pos
-    until stone_count == 0
+    until stones.empty?
       current_cup += 1
       if current_cup > 13
-        current_cup == 0
+        current_cup = 0
       end
-      if (self.current_player == @player2 && current_cup == 13) || (self.current_player == @player1 && current_cup == 6)
-        self.cups[current_cup] << :stone
-        stone_count -= 1
+      # if current_cup == 6 && current_player_name == @player1
+      #   @cups[current_cup] << :stone
+      #   stone_count -= 1
+      # elsif current_cup == 13 && current_player_name == @player2
+      #   @cups[current_cup] << :stone
+      #   stone_count -= 1
+      # else
+      #   @cups[current_cup] << :stone
+      #   stone_count -= 1
+      if current_cup == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif current_cup == 13
+        @cups[13] << stones.pop if current_player_name == @name2
       else
-        self.cups[current_cup] << :stone
-        stone_count -= 1
-      end
+        @cups[current_cup] << stones.pop
+  end
     end
     render
     next_turn(current_cup)
@@ -74,8 +84,18 @@ class Board
   end
 
   def one_side_empty?
+    cups.take(6).all? { |cup| cup.empty? } ||
+    cups[7..12].all? { |cup| cup.empty? }
   end
 
   def winner
+    player1_score = @cups[6].count
+    player2_score = @cups[13].count
+
+    if player1_score == player2_score
+      :draw
+    else
+      player1_score > player2_score ? @name1 : @name2
+    end 
   end
 end
